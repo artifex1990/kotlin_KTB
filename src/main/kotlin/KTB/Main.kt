6eeff12111f8c0abeea9d7b5c22d686a1fc602ce
pavel demyanov <pavel@demjanov.ru>
2025-08-03 01:30:25 +0300

    KTB-07-random-words: Реализовать функционал изучения слов Что сделано:
    - [X] Проинициализировать переменную со списком невыученных слов (notLearnedList).
    - [X] Добавить проверку на наличие невыученных слов. Если все слова выучены – выводить сообщение “Все слова в словаре выучены” и выходить в меню.
    - [X] Создать и проинициализировать переменную со списком слов для изучения (questionWords). Список должен содержать 4 невыученных слова. Рекомендуется использовать метод take(). Для перемешивания списка рекомендуется использовать метод shuffled().
    - [X] В режиме изучения слов отображать одно загаданное (correctAnswer) слово и четыре варианта ответа в случайном порядке. После отображения ожидать ввод номера ответа от пользователя.

--- src/main/kotlin/KTB/Main.kt
+++ src/main/kotlin/KTB/Main.kt
@@ -4,6 +4,7 @@ import java.io.File
 
 const val SEPARATOR = '|'
 const val MAX_WORDS_FOR_KNOWS = 3
+const val MAX_WORDS_FOR_EXERCISE = 4
 
 data class Word(
     val original: String,
@@ -26,7 +27,46 @@ fun main() {
 
         print("Введите пункт: ")
         when (readln()) {
-            "1" -> println("Учить слова")
+            "1" -> {
+                while (true) {
+                    println()
+                    val notLearnedList = dictionary.filter { word -> word.correctAnswersCount < MAX_WORDS_FOR_KNOWS }
+
+                    if (notLearnedList.isEmpty()) {
+                        println("Все слова в словаре выучены")
+                        break
+                    }
+
+                    val questionWords = notLearnedList.shuffled().take(MAX_WORDS_FOR_EXERCISE)
+                    val correctAnswer = questionWords.random()
+
+                    println("${correctAnswer.original}:")
+                    questionWords.forEachIndexed { i, word ->
+                        println("${i + 1} - ${word.translate}")
+                    }
+                    println("0 - выход")
+
+                    print("Введите ваш ответ: ")
+                    val answer = readln().toInt()
+
+                    when {
+                        answer == 0 -> break
+
+                        answer !in 1..MAX_WORDS_FOR_EXERCISE -> {
+                            println("Не правильный ввод!")
+                            continue
+                        }
+
+                        correctAnswer.original != questionWords[answer - 1].original -> {
+                            println("Не правильное слово! Правильное ${correctAnswer.translate}")
+                            continue
+                        }
+
+                        else -> println("Правильный ответ!")
+                    }
+                }
+            }
+
             "2" -> {
                 println("Статистика")
                 val learnedCount = dictionary.filter { word -> word.correctAnswersCount >= MAX_WORDS_FOR_KNOWS }.size
@@ -53,6 +93,8 @@ fun loadDictionary(): List<Word> {
 
         fileWords.appendText("hello${SEPARATOR}привет${SEPARATOR}0\n")
         fileWords.appendText("dog${SEPARATOR}собака\n")
+        fileWords.appendText("fish${SEPARATOR}рыба${SEPARATOR}0\n")
+        fileWords.appendText("chicken${SEPARATOR}курица${SEPARATOR}0\n")
         fileWords.appendText("cat${SEPARATOR}кошка${SEPARATOR}0\n")
     }
 
