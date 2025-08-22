3ccb05ebbda9ff2e4e8556dea30a9c3fdc1730e9
stokato <demyanovpaul@yandex.ru>
2025-08-22 20:26:29 +0300

    KTB-15-send-message: Отправить сообщение пользователю через Telegram API
    - [X] Создать отдельную функцию для отправки сообщений (sendMessage), аналогичную getUpdates, которая принимает chatId.
    - [X] Разработать новое регулярное выражение (RegEx) для извлечения chatId из полученных обновлений.
    - [X] Реализовать функциональность: при отправке в бота сообщения 'Hello' он должен отвечать тем же сообщением 'Hello'.
    - [X] Провести рефакторинг кода: создать отдельный класс TelegramBotService с методами для взаимодействия с Telegram API (на данный момент у нас есть 2 метода).

--- src/main/kotlin/Telegram/Main.kt
+++ src/main/kotlin/Telegram/Main.kt
@@ -10,19 +10,40 @@ fun main(args: Array<String>) {
     println(telegramBotService.getMe())
 
     println("GetUpdates")
-    val updateIdRegex: Regex = "\"update_id\":([0-9]+)".toRegex()
-    
-    while(true) {
+    val updateIdRegex: Regex = "\"update_id\":([0-9]+)[,}]".toRegex()
+    val chatIdRegex: Regex = "\"chat\":\\{[^}]*\"id\":([0-9]+)".toRegex()
+    val chatTextRegex: Regex = "\"text\":\"([^\"]*)\"".toRegex()
+
+    while (true) {
         Thread.sleep(MAX_TIME_SLEEP)
         val updates: String = telegramBotService.getUpdates(updateId)
-        
-        val matchResult: MatchResult? = updateIdRegex.find(updates)
-        val groupsString = matchResult?.groups?.get(1)?.value
+
+        val groupUpdateId = findOnRegex(updateIdRegex, 1, updates)
+        val groupChatId = findOnRegex(chatIdRegex, 1, updates)
+        val groupChatText = findOnRegex(chatTextRegex, 1, updates)
+            ?.let { unescapeJsonText(it) }
 
         println(updates)
 
-        if (groupsString.isNullOrEmpty()) continue
+        if (
+            groupUpdateId.isNullOrEmpty() ||
+            groupChatId.isNullOrEmpty() ||
+            groupChatText.isNullOrEmpty()
+        ) continue
 
-        updateId = groupsString.toInt() + 1
+        println(telegramBotService.sendMessage(groupChatId, groupChatText))
+
+        updateId = groupUpdateId.toInt() + 1
     }
 }
+
+fun findOnRegex(regexTemplate: Regex, groupId: Int, updates: String): String? =
+    regexTemplate.find(updates)?.groups?.get(groupId)?.value
+
+fun unescapeJsonText(text: String): String =
+    text
+        .replace("\\\\u([0-9a-fA-F]{4})".toRegex()) {
+            it.groupValues[1].toInt(16).toChar().toString()
+        }
+        .replace("\\\"", "\"")
+        .replace("\\\\", "\\")
